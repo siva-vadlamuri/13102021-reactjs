47b61d1cfc7b4d41551017fcdef041614863a866
siva-vadlamuri <sivavadlamuru1997@gmail.com>
2022-01-21 07:25:43 +0530

    react-router-dom-added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-ReactDOM.render(<App name="shiva" />, document.getElementById("root"));
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 // ./ same directory
 //  ../ represents the previous directory
 //  ../../../../../
