12a1927b0bd3524322e541550d7294754222c9c2
siva-vadlamuri <sivavadlamuru1997@gmail.com>
2022-01-24 09:21:11 +0530

    introduction-to-hooks

---
+++ src/components/hooks/Counter.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+function Counter() {
+  const [count, setCount] = useState(0);
+  //   Dont call the hooks in class components
+  // Dont call the hooks in loops,condtions,nested functions
+  // if(count>2){
+  //     const [countTwo,setCountTwo] = useState(count);
+  // }
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  const handleDecrement = () => (count > 0 ? setCount(count - 1) : null);
+
+  return (
+    <div>
+      <h2>Counter Application</h2>
+      <p> count is :{count}</p>
+      <button onClick={handleIncrement} className="btn btn-info px-3 py-1">
+        Increment
+      </button>
+      <button onClick={handleDecrement} className="btn btn-info px-3 py-1 ml-2">
+        decrement
+      </button>
+    </div>
+  );
+}
+export default Counter;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Counter from '../hooks/Counter';
 
  const Home = () => {
   return <div>
       <h2>
         Home Page
+        <Counter/>
       </h2>
   </div>;
 };
